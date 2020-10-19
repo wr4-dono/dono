6a88a78e55349252899c4b2e2e1a4bd6c372d715
Jake Butters <jacobbutters@gmail.com>
2020-10-19 15:15:38 -0600

    donoController complete other than Amazon S3 integration

---
+++ db/createDono.sql
@@ -0,0 +1,4 @@
+INSERT INTO donos
+(giver_id, zip_code, title, description, price, multiple_people, truck_trailer)
+VALUES
+($1,$2,$3,$4,$5,$6,$7);
--- server/index.js
+++ server/index.js
@@ -2,49 +2,52 @@ require('dotenv').config()
 const express = require('express')
 const session = require('express-session')
 const massive = require('massive')
+const app = express()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
+const donoCtrl = require('../server/donoController')
 
-const app = express()
 
-const {CONNECTION_STRING, SERVER_PORT, SESSION_SECRET} = process.env
+const { CONNECTION_STRING, SERVER_PORT, SESSION_SECRET } = process.env
 
 app.use(express.json())
 app.use(
-    session({
-        secret: SESSION_SECRET,
-        resave: false,
-        saveUninitialized: true,
-        cookie: {maxAge: 1000 * 60 * 60 * 24 * 365}
-    })
+  session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 365 }
+  })
 )
 
-
-
+app.get('/api/donos', donoCtrl.getAllDonos);
+app.get('/api/donos/:dono_id', donoCtrl.getDono);
+app.post('/api/donos/', donoCtrl.createDono);
+app.put('/api/users/:user_id/dono/:dono_id', donoCtrl.acceptDono);
+app.put('/api/donos/:dono_id', donoCtrl.editDono);
+app.delete('/api/donos/:dono_id', donoCtrl.deleteDono)
 
 massive({
-    connectionString: CONNECTION_STRING,
-    ssl: {rejectUnauthorized: false},
+  connectionString: CONNECTION_STRING,
+  ssl: { rejectUnauthorized: false },
 }).then(dbInstance => {
-    app.set('db', dbInstance)
-    console.log('DB ready!')
-    app.listen(SERVER_PORT, () =>
+  app.set('db', dbInstance)
+  console.log('DB ready!')
+  app.listen(SERVER_PORT, () =>
     console.log(`Server ready on port ${SERVER_PORT}`)
-    )
+  )
 })
 
 
-
-
 io.on('connection', socket => {
-    socket.on('message', ({ name, message }) => {
-      io.emit('message', { name, message })
-    })
+  socket.on('message', ({ name, message }) => {
+    io.emit('message', { name, message })
   })
+})
 
-  http.listen(4000, function() {
-    console.log('listening on port 4000')
-  })
+http.listen(4000, function () {
+  console.log('listening on port 4000')
+})
 
 
 
