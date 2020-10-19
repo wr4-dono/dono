510c9f8b9434de7833b4827172951cf20c624196
Carson Davies <carsondavies@Carsons-MBP.T-mobile.com>
2020-10-19 10:07:03 -0600

    added authReducer and donoReducer

---
+++ src/ducks/authReducer.js
@@ -0,0 +1,51 @@
+import axios from 'axios'
+
+const initialState = {
+  user: {},
+  isLoggedIn: false
+}
+
+const LOGIN_USER = 'LOGIN_USER'
+const LOGOUT_USER = 'LOGOUT_USER'
+const GET_USER = 'GET_USER'
+
+export function loginUser(user) {
+  return {
+    type: LOGIN_USER,
+    payload: user
+  }
+}
+
+export function logoutUser() {
+  return {
+    type: LOGOUT_USER,
+    payload: null
+  }
+}
+
+export function getUser() {
+  const payload = axios.get('/auth/user')
+
+  return {
+    type: GET_USER,
+    payload: payload
+  }
+}
+
+export default function (state = initialState, action) {
+  console.warn(state)
+  switch (action.type) {
+    case LOGIN_USER:
+      return { ...state, user: action.payload, isLoggedIn: true }
+    case LOGOUT_USER:
+      return initialState
+    case GET_USER + '_PENDING':
+      return { ...state }
+    case GET_USER + '_FULFILLED':
+      return { ...state, user: action.payload.data, isLoggedIn: true }
+    case GET_USER + '_REJECTED':
+      return initialState
+    default:
+      return state
+  }
+}
