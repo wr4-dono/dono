11becdac90b4b73118179e02b0c11de783133f10
Carson Davies <carsondavies@Carsons-MBP.T-mobile.com>
2020-10-19 11:30:56 -0600

    set up files as functional components

---
+++ src/Components/Favorites/Favorites.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Favorites = () => {
+
+  return (
+    <div>Favorites.js
+    Maps over donos array on redux renders dono thumbnail component for each array item that has been favorited by user.
+    </div>
+  )
+}
+
+export default Favorites
