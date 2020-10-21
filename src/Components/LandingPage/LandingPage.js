9436780ad7630561be5a0ce8a54857d6aeafc48d
Jake Butters <jacobbutters@gmail.com>
2020-10-20 21:45:05 -0600

    fixed LandingPage, front end ZipCode portion working

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -1,19 +1,26 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { connect } from 'react-redux'
+import DonoThumbnail from '../DonoThumbnail/DonoThumbnail'
 
 const LandingPage = (props) => {
-  const [radius, setRadius] = useState(null)
+  const [radius, setRadius] = useState('')
   //set radius determined by input by user or potentially drop down selection.
   const [donos, setDonos] = useState([])
 
   useEffect(() => {
-    axios.get(`/api/donos?status=1&zip_code=${props.user.zip_code}&radius=${radius}`)
-      .then((res) => setDonos(res.data))
-  }, [props.user.zip_code, radius])
+    const { zip_code } = props.auth.user
+    console.log(radius)
+    axios.get(`/api/donos?status=1&zip_code=${zip_code}&radius=${radius}`)
+      .then((res) => setDonos(res.data)).catch(err => console.log(err.message))
+  }, [props.auth.user.zip_code, radius])
+
+
 
   return (
-    <div>LandingPage.js
+    <div>
+      <input type='number' placeholder='Distance In Miles' onChange={(e) => setRadius(e.target.value)}></input>
+      {/* <button onClick={()=> submitDistance()}>Get Donos</button> */}
       {donos.map(dono => {
         return <DonoThumbnail dono={dono} />
       })}
