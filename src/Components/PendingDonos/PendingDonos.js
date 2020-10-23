2244704346393145d0793426c47a028cbb28237c
namantia1@gmail.com <namantia1@gmail.com>
2020-10-23 10:48:33 -0400

    pending donos

--- src/Components/PendingDonos/PendingDonos.js
+++ src/Components/PendingDonos/PendingDonos.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import { connect } from 'react-redux'
+import DonoThumbnail from '../DonoThumbnail/DonoThumbnail'
 
 const PendingDonos = () => {
+  const [pendingDonos, setPendingDonos] = useState([])
+
+    useEffect(() => {
+      axios.get(`/api/donos?status=2`)
+      .then((res) => setPendingDonos(res.data)).catch(err => console.log(err.message))
+
+    }, [])
 
   return (
-    <div> PendingDonos.js </div>
+    <div> PendingDonos.js 
+    {pendingDonos.map(dono => {
+      return < DonoThumbnail pendingDonos={dono} />
+    })}
+
+    </div>
   )
 }
 
-export default PendingDonos
+const mapStateToProps = reduxState => reduxState
+
+export default connect(mapStateToProps)(PendingDonos)
