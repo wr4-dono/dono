1a6d5feb4d9a3b688748125e89aa5db037b40b90
Jake Butters <jacobbutters@gmail.com>
2020-10-27 11:08:03 -0600

    fixed user_state on profile, and desplaying username on rateuser

--- server/profileController.js
+++ server/profileController.js
@@ -1,13 +1,12 @@
 module.exports = {
     editInfo: async (req, res) => {
         const db = req.app.get('db')
-        const{username, zip_code, email} = req.body
+        const { username, user_state, zip_code, email } = req.body
 
-        //pulls if for user from thier session
-        const {user_id} = req.session.user
+        //pulls if for user from session
+        const { user_id } = req.session.user
 
-        const [updateProfile] = await db.edit_profile([user_id, username, zip_code, email])
-            res.status(200).send(updateProfile)
-
+        const [updateProfile] = await db.edit_profile([user_id, username, user_state, zip_code, email])
+        res.status(200).send(updateProfile)
     }
 }
