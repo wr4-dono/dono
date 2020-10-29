9f0997f59c6783716e09fabe3c05efeb13153fd3
Carson Davies <carsond11@gmail.com>
2020-10-29 13:53:05 -0600

    pending ratings button styled

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -53,7 +53,7 @@ class Header extends Component {
 
         <Link className="dono" to="/Landing"> <img className="dono" src="https://i.imgur.com/tuFQHxN.png" /></Link>
         {(this.state.pendingRatings) ?
-            <Link  to='/PendingRatings'> <button className='header-pending'><p className='pending-number'>
+            <Link style={{textDecoration:'none'}} to='/PendingRatings'> <button className='header-pending'><p  className='pending-number'>
               {this.state.pendingRatings}
             </p></button>
             </Link> : null}
